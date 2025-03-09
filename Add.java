399191aeb2f92485c74f04c0ca67ebb269a715f7
brainmentorspvtltd <amitsrivastava@Amits-MacBook-Pro-2.local>
2025-03-10 01:55:04 +0530

    done

--- Add.java
+++ Add.java
@@ -1,12 +1,13 @@
 public class Add {
-   // Command Line Arguments 
+   // Command Line Arguments
    public static void main(String[] args) {
       // System.out.println(args[0]);
       // System.out.println(args[1]);
       int sum = 0;
-      for(int i = 0; i<args.length; i++){
+      for (int i = 0; i < args.length; i++) {
          sum = sum + Integer.parseInt(args[i]);
       }
       System.out.println(sum);
-   } 
+   }
+   // I want to do some changes
 }
